c71d3b6cf682591513190ff102ab73cce62bb6b3
Anna Walaszek <annnevvv@noreplay.github.com>
2023-05-30 00:41:23 +0200

    Adding views (BlogMainPageView, AllPostView) to 'blog' application.

--- F1/blog/views.py
+++ F1/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
+
+from .models import Post
 
 # Create your views here.
+
+all_posts = Post.objects.all().order_by('-date')
+
+
+class BlogMainPageView(ListView):
+    template_name = "blog/blog_main.html"
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+class AllPostView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
