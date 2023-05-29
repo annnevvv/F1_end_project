af529a9524a53f5e7478bd0768682e7eb51c0656
Anna Walaszek <annnevvv@noreplay.github.com>
2023-05-29 22:31:21 +0200

    Adding admin panel to the 'blog' application.

--- F1/blog/admin.py
+++ F1/blog/admin.py
@@ -2,3 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Author, Post, Comment, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('author', 'tags')
+    list_display = ('title', 'author', 'date')
+    prepopulated_fields = {'slug': ("title",)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('post',)
+    list_display = ('user_name', 'user_email')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
