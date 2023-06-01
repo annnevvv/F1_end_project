5f6f9b899c2003fc9d6d417861b9889c8ebe08e5
Dar1us7 <d.nowosielski1993@gmail.com>
2023-06-01 20:34:47 +0200

    posts added and bug fixes

--- F1/F1/urls.py
+++ F1/F1/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls'), name='blog'),
--- F1/blog/urls.py
+++ F1/blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from blog.views import BlogMainPageView, AllPostView, SinglePostView
@@ -5,7 +7,6 @@ from blog.views import BlogMainPageView, AllPostView, SinglePostView
 urlpatterns = [
     path('', BlogMainPageView.as_view(), name='blog_main_page'),
     path('posts', AllPostView.as_view(), name='posts_list'),
-    path("posts/<slug:slug>", SinglePostView.as_view(),
-         name='single_post_page'),
+    path("posts/<slug:slug>", SinglePostView.as_view(),name='post-detail-page'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
