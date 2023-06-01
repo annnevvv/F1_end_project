959ffa796cbee9fc3318be2f9e5dc82832d8cdbf
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-01 15:15:38 +0200

    Refactoring urls.py localization

--- F1/blog/urls.py
+++ F1/blog/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path, include
+
+from F1.blog.migrations import views
+
+urlpatterns = [
+    path('', views.BlogMainPageView.as_view(), name='blog_main_page'),
+    path('posts', views.AllPostView.as_view(), name='posts_list'),
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+         name='single_post_page'),
+
+]
