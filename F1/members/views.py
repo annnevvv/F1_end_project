d41b7a7e28d3d73bd0bb50f047c51faf2aa18419
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-06 05:03:11 +0200

    Added Views to members app

---
+++ F1/members/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+from .forms import RegistrationForm
+from blog.models import Post
+
+
+# Create your views here.
+
+
+def goToMemberRegister(request):
+    return render(request, 'members/main/members.html')
+
+
+def memberRegisterConfirmation(request):
+    return render(request, 'members/main/confirm-memeber-account-created.html')
+
+
+@login_required(login_url='/login')
+def memberdashboard(request):
+    posts = Post.objects.all()
+
+    if request.method == 'POST':
+        post_id = request.POST.get('post-id')
+        post = Post.objects.get(id=post_id)
+        if post and post.user == request.user:
+            post.delete()
+
+    return render(request, 'members/main/dashboard.html', {'posts': posts})
+
+
+def signUp(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=True)
+            login(request, user)
+            return redirect('confirm-memeber-account-created')
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/sign-up.html', {'form': form})
