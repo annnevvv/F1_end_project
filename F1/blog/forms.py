17bf7b768e22956f962e6fcf35710e74654755fd
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-06 04:29:15 +0200

    Added PostCreatedForm

--- F1/blog/forms.py
+++ F1/blog/forms.py
@@ -13,3 +13,10 @@ class CommentForm(forms.ModelForm):
             'txt': 'Your Comment'
         }
         # fields = ['user_name', 'user_email', 'txt', 'post']
+
+
+class PostCreatedForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'excerpt', 'txt', 'slug']
+        # prepopulated = ['slug']
