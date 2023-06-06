4a55e78426019d2b1470c44c7e1057f4e1c5485f
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-06 16:52:47 +0200

    Added create-post url

--- F1/blog/urls.py
+++ F1/blog/urls.py
@@ -1,1 +1,13 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import BlogMainPageView, AllPostView, SinglePostView, createPost
+
+urlpatterns = [
+    path('', BlogMainPageView.as_view(), name='blog_main_page'),
+    path('posts', AllPostView.as_view(), name='posts_list'),
+    path("posts/<slug:slug>", SinglePostView.as_view(), name='post-detail-page'),
+    path('create-post', createPost, name='create_post'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
