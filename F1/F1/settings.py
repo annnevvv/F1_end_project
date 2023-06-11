fdd5b049e18c9234adb1dcc5004f3fc083923a3b
Anna Walaszek <a.e.walaszek@gmail.com>
2023-06-11 19:05:23 +0200

    Adding login redirect url to settings

--- F1/F1/settings.py
+++ F1/F1/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/dashboard'
+LOGIN_REDIRECT_URL = '/members/dashboard'
 
 LOGOUT_REDIRECT_URL = '/homepage'
