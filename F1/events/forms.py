eca9d466c791f7b3f0940bfb9b55f9da7c105dea
Anna Walaszek <a.e.walaszek@gmail.com>
2023-06-11 19:54:40 +0200

    Adding other form to events for login users

--- F1/events/forms.py
+++ F1/events/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from django.contrib.auth.models import User
 
-from .models import Participant
+
+# class RegistrationForm(forms.Form):
+#     email = forms.EmailField(label='Your email')
 
 
-class RegistrationForm(forms.Form):
-    email = forms.EmailField(label='Your email')
+class RegistrationForEventForm(forms.Form):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
