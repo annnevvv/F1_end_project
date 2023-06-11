afac429bc37b57886e328d7e50e3d38efdf013d6
Anna Walaszek <a.e.walaszek@gmail.com>
2023-06-11 19:58:58 +0200

    Adding code changes to admin.py

--- F1/events/admin.py
+++ F1/events/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Event, Circuit, Participant
+from .models import Event, Circuit
 
 # Register your models here.
 
@@ -11,4 +11,3 @@ from .models import Event, Circuit, Participant
 
 admin.site.register(Event)
 admin.site.register(Circuit)
-admin.site.register(Participant)
