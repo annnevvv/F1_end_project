51d8ddf46d60fef37199ecad26750fa13a8b8638
Anna Walaszek <a.e.walaszek@gmail.com>
2023-06-11 18:59:07 +0200

    Adding changes to views

--- F1/members/views.py
+++ F1/members/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 
 from .forms import RegistrationForm
 from blog.models import Post
+from events.models import Event
 
 
 # Create your views here.
@@ -18,16 +20,18 @@ def memberRegisterConfirmation(request):
 
 
 @login_required(login_url='/login')
+@permission_required('blog.delete_post', login_url='/login', raise_exception=True)
 def memberDashboard(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-date")
+    events = Event.objects.all().order_by("-date")
 
     if request.method == 'POST':
         post_id = request.POST.get('post-id')
         post = Post.objects.get(id=post_id)
-        if post and post.user == request.user:
+        if post and (post.user == request.user or request.user.has_perm('blog.delete_post')):
             post.delete()
 
-    return render(request, 'members/main/dashboard.html', {'posts': posts})
+    return render(request, 'members/main/dashboard.html', {'posts': posts, 'events': events})
 
 
 def signUp(request):
@@ -36,6 +40,10 @@ def signUp(request):
         if form.is_valid():
             user = form.save(commit=True)
             login(request, user)
+
+            default_group = Group.objects.get(name='default')
+            user.groups.add(default_group)
+
             return redirect('confirm-memeber-account-created')
 
     else:
