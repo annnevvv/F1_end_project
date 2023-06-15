7cc75ea09778f2087542dd0c78076923efc3841c
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-15 02:05:58 +0200

    Adding test to blog post create form

--- F1/blog/forms.py
+++ F1/blog/forms.py
@@ -12,21 +12,12 @@ class CommentForm(forms.ModelForm):
             'user_email': 'Your Email',
             'txt': 'Your Comment'
         }
-        # fields = ['user_name', 'user_email', 'txt', 'post']
+
 
 
 class PostCreatedForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'excerpt', 'txt', 'slug']
-        # prepopulated = ['slug']
 
-        # def clean_slug(self):
-        #     slug = self.cleaned_data['slug']
-        #     if not slug:
-        #         title = self.cleaned_data['title']
-        #         slug = slugify(title)
-        #     return slug
-        #
-        # fields = ['title', 'excerpt', 'txt', 'slug']
 
