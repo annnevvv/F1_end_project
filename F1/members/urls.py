ed6803ff23f92433e4a795a596e438f3eeb8caa7
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-21 11:34:25 +0200

    Changed views function names refactor

--- F1/members/urls.py
+++ F1/members/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import goToMemberRegister, signUp, memberRegisterConfirmation, \
-    memberDashboard, deleteMemberAccount
+from .views import go_to_member_register, signUp, member_register_confirmation, \
+    member_dashboard, delete_member_account
 
 urlpatterns = [
 
-    path('', goToMemberRegister, name='go_to_register_or_login'),
+    path('', go_to_member_register, name='go_to_register_or_login'),
     path('signup', signUp, name='sign_up'),
-    path('confirm-member-account-created', memberRegisterConfirmation,
+    path('confirm-member-account-created', member_register_confirmation,
          name='confirm-member-account-created'),
-    path('delete-member-account', deleteMemberAccount,
+    path('delete-member-account', delete_member_account,
          name='delete_member_account'),
 
     path('password-change-done', auth_views.PasswordChangeDoneView.as_view(
@@ -22,6 +22,6 @@ urlpatterns = [
     path('password-reset', auth_views.PasswordResetView.as_view(
         template_name='password-reset.html')),
 
-    path('dashboard', memberDashboard, name='member_dashboard'),
+    path('dashboard', member_dashboard, name='member_dashboard'),
 
 ]
