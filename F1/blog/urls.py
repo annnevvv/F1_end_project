9f4dff38eb4ebeb8b71d7a9a7602589cff6d502e
Anna Walaszek <annnevvv@noreplay.github.com>
2023-06-22 18:31:12 +0200

    Syntax fix

--- F1/blog/urls.py
+++ F1/blog/urls.py
@@ -2,16 +2,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import BlogMainPageView, AllPostView, SinglePostView, \
-    createPost, postCreatedConfirmation
+from .views import BlogMainPageView, AllPostView, SinglePostView, create_post, PostCreatedConfirmationView
 
 urlpatterns = [
                   path('', BlogMainPageView.as_view(), name='blog_main_page'),
                   path('posts', AllPostView.as_view(), name='posts_list'),
                   path("posts/<slug:slug>", SinglePostView.as_view(),
                        name='post-detail-page'),
-                  path('create-post', createPost, name='create_post'),
-                  path('create-post-confirm', postCreatedConfirmation,
+                  path('create-post', create_post, name='create_post'),
+                  path('create-post-confirm', PostCreatedConfirmationView.as_view(),
                        name='create_post_confirmation'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
